180ae0a8a7d194860e01366f028cfc02807ee585
ALargeBagel <aaron.tkd@hotmail.com>
2015-10-10 13:44:39 +0100

    Dissect added

---
+++ src/Dissect.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Aaron on 10/10/2015.
+ */
+public class Dissect {
+    private ArrayList<String> words;
+    private String q;
+
+    public Dissect(String q){
+        words = new ArrayList<String>();
+        this.q = q;
+
+        process(q);
+    }
+
+    public void process(String q){
+        String[] arr = q.split(" ");
+
+        for ( String ss : arr) {
+
+            System.out.println(ss);
+        }
+    }
+
+    public ArrayList<String> getWords() {
+        return words;
+    }
+}
---
+++ src/Dissected.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Aaron on 10/10/2015.
+ */
+public interface Dissected {
+    public ArrayList<String> getWords();
+}
